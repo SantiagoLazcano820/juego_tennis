ccc8c54a1139afcbe1700e71abf91475539b1965
Santiago Lazcano Quiroga <santiagolazcanoquiroga@MacBook-Air-de-Santiago.local>
2026-09-03 01:17:30 -0400

    feat: Mostrar Advantage for Player 1 cuando jugador 1 anota 4 veces y jugador 2 anota 3 veces

--- src/tennis.js
+++ src/tennis.js
@@ -13,6 +13,10 @@ class Tennis {
   }
 
   obtenerScore() {
+    if (this.puntosJugador1 === 4 && this.puntosJugador2 === 3) {
+      return "Advantage for Player 1";
+    }
+    
     if (this.puntosJugador1 === 4) {
       return "Game for Player 1";
     }
--- src/tennis.spec.js
+++ src/tennis.spec.js
@@ -84,4 +84,16 @@ describe("Tennis", () => {
     tennis.jugador2Anota();
     expect(tennis.obtenerScore()).toEqual("Deuce");
   });
+
+  it("deberia mostrar Advantage for Player 1 cuando jugador 1 anota 4 veces y jugador 2 anota 3 veces" , () => {
+    let tennis = new Tennis();
+    tennis.jugador1Anota();
+    tennis.jugador1Anota();
+    tennis.jugador1Anota();
+    tennis.jugador1Anota();
+    tennis.jugador2Anota();
+    tennis.jugador2Anota();
+    tennis.jugador2Anota();
+    expect(tennis.obtenerScore()).toEqual("Advantage for Player 1");
+  });
 });
